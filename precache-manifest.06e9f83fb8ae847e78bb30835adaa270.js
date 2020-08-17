aa11fb020217c6bc7e57c533c80ffd2e79c5f2e3
wenzelpaul <wenzelpleyer@gmail.com>
2020-08-17 12:46:40 +0200

    Updates

---
+++ precache-manifest.06e9f83fb8ae847e78bb30835adaa270.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce16709a14bcebaf2cc241e9868deeb",
+    "url": "/casa-orizzonteblu/index.html"
+  },
+  {
+    "revision": "5892898f9adacabf6bac",
+    "url": "/casa-orizzonteblu/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8a6636a49982c74b07b",
+    "url": "/casa-orizzonteblu/static/js/2.c03a9fc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/casa-orizzonteblu/static/js/2.c03a9fc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5892898f9adacabf6bac",
+    "url": "/casa-orizzonteblu/static/js/main.e3b89abe.chunk.js"
+  },
+  {
+    "revision": "9011062fd1e911a760d1",
+    "url": "/casa-orizzonteblu/static/js/runtime-main.9f39b9cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/casa-orizzonteblu/static/media/logo.5d5d9eef.svg"
+  }
+]);
